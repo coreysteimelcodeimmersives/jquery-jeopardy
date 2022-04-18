86c057d4cb090a2ea245b3404ee596a22f90132e
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-04-18 14:14:53 -0600

    deleting console logs

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
 async function readJeopardyData() {
     let rawJeopardyData = await fetch('jeopardy.json');
     let jeopardyData = await rawJeopardyData.json();
-    console.log(jeopardyData);
-    // group data by date
     let dataByDate = _.groupBy(jeopardyData, 'date');
-    console.log(dataByDate)
-    console.log(typeof (dataByDate));
-    // next group date data by round for each date
-
     for (let key in dataByDate) {
-        console.log(key, dataByDate[key]);
-        let roundData = _.groupBy(dataByDate[key], 'round');
-        console.log(roundData);
+        let roundData = _.groupBy(dataByDate[key], 'round'); 
         dataByDate[key] = roundData;
         for (let key in roundData){
             let categoryData = _.groupBy(roundData[key], 'category');
