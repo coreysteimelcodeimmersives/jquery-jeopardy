87014144a2582ca8401b083802b3287f88f505e0
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-04-18 13:54:04 -0600

    getting my data sorted by category by date

--- main.js
+++ main.js
@@ -2,8 +2,28 @@ async function readJeopardyData() {
     let rawJeopardyData = await fetch('jeopardy.json');
     let jeopardyData = await rawJeopardyData.json();
     console.log(jeopardyData);
-    let groupData = _.groupBy(jeopardyData, 'date');
-    console.log(groupData)
+    // group data by date
+    let dataByDate = _.groupBy(jeopardyData, 'date');
+    console.log(dataByDate)
+    console.log(typeof (dataByDate));
+    // next group date data by round for each date
+
+    for (let key in dataByDate) {
+        console.log(key, dataByDate[key]);
+        let roundData = _.groupBy(dataByDate[key], 'round');
+        console.log(roundData);
+        dataByDate[key] = roundData;
+    }
+
+    console.log(dataByDate)
+
+
+    // for(let value of Object.values(dataByDate)){
+    //     // console.log(value);
+    //     let roundData = _.groupBy(value, 'round');
+
+    //   }
+
 }
 
 readJeopardyData();
